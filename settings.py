bb30cbd81d406d7ec50204889a2c03be778e3ea1
perchouli <jp.chenyang@gmail.com>
2012-01-15 19:37:14 +0800

    New branch

--- settings.py
+++ settings.py
@@ -1,8 +1,4 @@
 # Django settings for synclee project.
-import os
-
-SITE_DIR = os.path.split(os.path.realpath(__file__))[0]+"/"
-SITE_URL = 'http://127.0.0.1:8000/'
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -15,18 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': 'synclee',                      # Or path to database file if using sqlite3.
-        'NAME': 'database.db',
-        'USER': 'root',                      # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        'OPTIONS': {
-   #'init_command': 'SET storage_engine=INNODB',
-}
-    
     }
 }
 
@@ -37,11 +27,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Asia/Shanghai'
+TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'zh-cn'
+LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
@@ -55,7 +45,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(SITE_DIR, 'uploads')
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -66,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SITE_DIR, 'static')
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -82,7 +72,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
 )
 
 # List of finder classes that know how to find static files in
@@ -94,7 +83,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'em@sv0fqj(c46^15o@4!@)%_+4up_*8v=&fj0ng(u90@^-^agh'
+SECRET_KEY = 'r@plq207#pab8y-p&6e+sts8)_+pphg91j0egj#nr1d*=_xi9v'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -107,59 +96,29 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'synclee.urls'
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.request",
-)
-
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    SITE_DIR+'./templates',
 )
 
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.comments',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'synclee.work',
-    'synclee.chapter',
-    'synclee.club',
-    'synclee.tab',
-    'synclee.message',
-    'synclee.profiles',
-    'synclee.tweet',
-    'synclee.search',
-    'synclee.billboard',
-    'synclee.recommand',
-    'synclee.general',
-    
-    'synclee.taggit',
-    'synclee.taggit_templatetags',
-    'synclee.templatetag_sugar',
-    'synclee.registration',
-    'synclee.notification',
-    'synclee.relationships',
-    'synclee.easy_thumbnails',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -184,10 +143,3 @@ LOGGING = {
         },
     }
 }
-
-AUTH_PROFILE_MODULE = 'profiles.UserProfiles'
-
-ACCOUNT_ACTIVATION_DAYS = 2
-#EMAIL_HOST = 'localhost'
-DEFAULT_FROM_EMAIL = 'noreply@synclee.net'
-LOGIN_REDIRECT_URL = '/'
--- urls.py
+++ urls.py
@@ -1,36 +1,17 @@
-# -*- coding: UTF-8 -*-
-
-from django.conf.urls.defaults import *
-from django.conf import settings
-from settings import SITE_DIR
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-import os
+from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
-#TODO:要禁止关键字注册用户名
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-    (r'^accounts/', include('synclee.registration.backends.default.urls')),
-    (r'^club/', include('synclee.club.urls')),
-    (r'^work/', include('synclee.work.urls')),
-    (r'^friends/', include('synclee.relationships.urls')),
-    (r'^message/', include('synclee.message.urls')),
-    (r'^comments/', include('django.contrib.comments.urls')),
-    (r'^notes/', include('synclee.notification.urls')),
-    (r'^tab/', include('synclee.tab.urls')),
-    (r'^tweet/',include('synclee.tweet.urls')),
-    (r'^board/',include('synclee.billboard.urls')),
-    (r'^', include('synclee.general.urls')),
-    (r'^', include('synclee.search.urls')),
-    (r'^', include('synclee.profiles.urls')),
-    
-    (r'^style/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.dirname(globals()["__file__"])+'/static/style'}),
-    (r'^script/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.dirname(globals()["__file__"])+'/static/script'}),
-    (r'^rc/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.dirname(globals()["__file__"])+'/static/rc'}),
-    (r'^uploads/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.dirname(globals()["__file__"])+'/uploads'}),
+    # Examples:
+    # url(r'^$', 'synclee.views.home', name='home'),
+    # url(r'^synclee/', include('synclee.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
 )
