01fc747894baa297208f7c9cee7349931b3e5e73
perchouli <jp.chenyang@gmail.com>
2012-01-28 14:38:36 +0800

    Fix a bug in upload element image

--- tools/upload/views.py
+++ tools/upload/views.py
@@ -1,7 +1,10 @@
+from django.contrib.sessions.models import Session
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
-import os
+import os, time, random
 
 @csrf_exempt
 def check_existing(request):
@@ -11,16 +14,20 @@ def check_existing(request):
     return HttpResponse('0')
 
 @csrf_exempt
-def upload_image(request):
-    file_name = str(request.FILES['Filedata'].name);
-    file_path = 'media/'
-    try:
-        open(file_path+file_name, 'w')
-    except:
-        os.mkdir(file_path)
-        file_upload = open(file_path+file_name, 'w')
-    else:
-        file_upload = open(file_path+file_name, 'w')
+def upload_image(request, user_id):
+    file_ext = str(request.FILES['Filedata'].name).split('.')[-1]
+    
+    # dont use Django save_FOO_file, defined a new filename
+    file_name = time.strftime('%Y%m%d%H%M%S')
+    file_name = file_name + '_%d' % random.randint(0,100)
+    
+    upload_path = os.path.join('media', 'upload')
+    user_upload_folder = os.path.join(upload_path, user_id)
+
+    if not os.path.exists(user_upload_folder):
+        os.mkdir(user_upload_folder)
+        
+    file_upload = open( os.path.join(user_upload_folder, file_name+'.'+file_ext), 'w')
     file_upload.write(request.FILES['Filedata'].read())
     file_upload.close()
-    return HttpResponse('0')
+    return HttpResponse(file_name+'.'+file_ext)
--- tools/urls.py
+++ tools/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('tools',
     url(r'^upload/check_existing/$', 'upload.views.check_existing', name='check_existing'),
-    url(r'^upload/upload_image/$', 'upload.views.upload_image', name='upload_image'),
+    url(r'^upload/upload_image/(?P<user_id>\d+)/$', 'upload.views.upload_image', name='upload_image'),
     #url(r'^write_work/(?P<work_id>\d+)/$', 'write_work', name='write_work'),
 )
